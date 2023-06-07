3f718c98891f792ec13f9d16e06293e2133d21c9
Rishav <rishavj032@gmail.com>
2023-06-07 20:21:17 +0530

    Todo App

---
+++ TodoApp/todo/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Addtodo from "./Components/Addtodo";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Addtodo />
+     
+    </div>
+  );
+}
+
+export default App;
