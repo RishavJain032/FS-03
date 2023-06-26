b8b19d090b0cf0fa87cecf7e173d9b60f1bc8c0f
Rishav <rishavj032@gmail.com>
2023-06-26 23:10:41 +0530

    Movie App

---
+++ Context-Api-Assignment/contextapiassignment/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import MoviesPage from "./Components/MoviesPage";
+
+function App() {
+  return (
+    <div className="App">
+      <MoviesPage />
+      
+    </div>
+  );
+}
+
+export default App;
