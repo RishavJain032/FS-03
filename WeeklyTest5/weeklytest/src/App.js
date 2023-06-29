bb22fc915225f91975081f5df7ef8488d1623a62
Rishav <rishavj032@gmail.com>
2023-06-29 20:30:22 +0530

    Weekly Test

---
+++ WeeklyTest5/weeklytest/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Poll from './Components/Poll';
+import Timer from './Components/Timer';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Timer/>
+      <Poll/>
+    </div>
+  );
+}
+
+export default App;
---
+++ WeeklyTest5/weeklytest/src/Components/Poll.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { useRef } from 'react';
+import {useState,useEffect} from "react";
+
+const Poll = () => {
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(0);
+    const [timer,setTimer]=useState(10);
+
+         
+    
+  
+    useEffect(() => {
+      
+
+      const fetchPosts = async () => {
+        try {
+          const response = await fetch(
+            `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${page}`
+          );
+          const data = await response.json();
+          const newPosts = data.hits.map((post) => ({
+            title: post.title,
+            url: post.url,
+            createdAt: post.created_at,
+            author: post.author,
+          }));
+          setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+          setPage((prevPage) => prevPage + 1);
+         
+          
+        } catch (error) {
+          console.error('Error fetching posts:', error);
+        }
+      };
+  
+      const intervalId = setInterval(fetchPosts, 10000);
+       
+     
+  
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [page]);
+  
+    return ( 
+
+        <>
+       
+      
+
+    {
+    posts.length>0 &&
+      <div>
+         <table width="100%" frame="box" rules="all" style={{boxShadow : "0 0 10px black"}} >
+          <thead>
+          <tr style={{backgroundColor : "black",color : "white" ,height : "30px"}}>
+              <th>Title</th>
+              <th>URL</th>
+              <th>Created At</th>
+              <th>Author</th>
+            </tr>
+          </thead>
+          <tbody align="center" padding = "10px" margin = "10px">
+            {posts.map((post, index) => (
+              <tr key={index}>
+                <td>{post.title}</td>
+                <td>{post.url}</td>
+                <td>{post.createdAt}</td>
+                <td>{post.author}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+}</>
+    );
+}
+
+export default Poll;
